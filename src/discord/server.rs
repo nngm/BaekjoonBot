b428472474403b698a9ce7b7554c8ef75825d433
stackbof <163146660+stackbof@users.noreply.github.com>
2025-07-25 00:00:00 +0000

    implement discord key verification

--- src/discord/server.rs
+++ src/discord/server.rs
@@ -1,3 +1,4 @@
+use ring::{error::Unspecified, signature};
 use serde_json::{self, json};
 use std::{
     io::{self, BufReader, BufWriter, prelude::*},
@@ -5,6 +6,53 @@ use std::{
 };
 
 use crate::http::{self, Method, server::RequestHeader};
+use super::ENV;
+
+fn parse_hex_str(string: &str) -> Option<Vec<u8>> {
+    let len = string.len();
+    if len % 2 != 0 {
+        return None;
+    }
+
+    let mut vec = Vec::with_capacity(len / 2);
+
+    for i in string.as_bytes().windows(2).step_by(2) {
+        vec.push(u8::from_str_radix(str::from_utf8(i).ok()?, 16).ok()?);
+    }
+
+    Some(vec)
+}
+
+fn verify_interaction(req_header: &RequestHeader, body: &[u8]) -> Result<(), Unspecified> {
+    let sig = parse_hex_str(
+        req_header
+            .headers
+            .iter()
+            .find(|&v| v.0 == "X-Signature-Ed25519")
+            .ok_or(Unspecified)?
+            .1
+            .as_str(),
+    )
+    .ok_or(Unspecified)?;
+
+    let time = req_header
+        .headers
+        .iter()
+        .find(|&v| v.0 == "X-Signature-Timestamp")
+        .ok_or(Unspecified)?
+        .1
+        .as_str();
+
+    let message = [time.as_bytes(), body].concat();
+
+    let key = signature::UnparsedPublicKey::new(
+        &signature::ED25519,
+        parse_hex_str(ENV.pubkey)
+            .ok_or(Unspecified)?,
+    );
+
+    key.verify(&message, &sig)
+}
 
 fn respond_ok_json(
     body_json: &serde_json::Value,
@@ -50,9 +98,11 @@ pub fn interactions(
         }
     }
 
-    let content: serde_json::Value =
-        serde_json::from_slice(http::server::read_content(&req_header, &mut reader)?.as_slice())?;
-    let interaction_type = content
+    let body = http::server::read_content(&req_header, &mut reader)?;
+    verify_interaction(&req_header, body.as_slice()).or::<io::Error>(Err(INVALID_INPUT.into()))?;
+
+    let object: serde_json::Value = serde_json::from_slice(body.as_slice())?;
+    let interaction_type = object
         .get("type")
         .ok_or::<io::Error>(INVALID_DATA.into())?
         .as_i64()
@@ -60,6 +110,8 @@ pub fn interactions(
 
     match interaction_type {
         INTERACTION_PING => pong(&mut writer),
-        _ => Err(INVALID_DATA.into()),
+        _ => {
+            Err(INVALID_DATA.into())
+        }
     }
 }
