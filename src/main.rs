ee9dcba37fe32edddf0ea0f69b08c7366aa08a6a
stackbof <163146660+stackbof@users.noreply.github.com>
2025-07-25 00:00:00 +0000

    cleanup main

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,5 @@ fn main() -> io::Result<()> {
     let routes: Vec<(&str, Handler)> =
         vec![("/api/v2/interactions", discord::server::interactions)];
     let server = Server::new("localhost:8765", routes.as_slice());
-    server.run()?;
-    Ok(())
+    server.run()
 }
